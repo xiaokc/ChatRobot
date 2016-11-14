755c3d96f0bae048fdea23e14954fb73e7df930f
xkc <1337154470@qq.com>
2016-11-14 20:19:08 +0800

    添加 注册和登录的url常量

--- app/src/main/java/com/xkc/chatrobot/Helper/Const.java
+++ app/src/main/java/com/xkc/chatrobot/Helper/Const.java
@@ -5,8 +5,12 @@ package com.xkc.chatrobot.Helper;
  */
 public class Const {
     public static final String turing_url = "http://www.tuling123.com/openapi/api";
+    public static final String local_server = "http://10.3.200.10:5000";
     public static final String key = "da89ff52e58c5ade5acbcb3c7bb35c8c";
     public static final String new_key = "3d89ec012826fbf85e7a018daec73026";
-    public static final int connect_timeout = 3000;
+    public static final int connect_timeout = 8000;
+    public static final String register_url = local_server + "/register";
+    public static final String login_url = local_server +"/login";
+
 
 }
