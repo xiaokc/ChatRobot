de249f3b10e305900e9087d6ca89c9268b83d477
xkc <1337154470@qq.com>
2016-11-14 20:20:33 +0800

    升级Android Studio相关的东西，如gradle版本

--- app/src/main/java/com/xkc/chatrobot/activity/SignInActivity.java
+++ app/src/main/java/com/xkc/chatrobot/activity/SignInActivity.java
@@ -1,8 +1,10 @@
 package com.xkc.chatrobot.activity;
 
+import android.app.Activity;
+
 /**
  * Created by xkc on 11/14/16.
  */
 
-public class SignInActivity {
+public class SignInActivity extends Activity {
 }
