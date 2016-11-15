ce3e9ba519be262d7c9a2d7ac9d57c04c544d8b1
xkc <1337154470@qq.com>
2016-11-15 10:51:16 +0800

    添加注册时用到的常量

--- app/src/main/java/com/xkc/chatrobot/Helper/Const.java
+++ app/src/main/java/com/xkc/chatrobot/Helper/Const.java
@@ -8,9 +8,14 @@ public class Const {
     public static final String local_server = "http://10.3.200.10:5000";
     public static final String key = "da89ff52e58c5ade5acbcb3c7bb35c8c";
     public static final String new_key = "3d89ec012826fbf85e7a018daec73026";
-    public static final int connect_timeout = 8000;
+    public static final int connect_timeout = 60000;
     public static final String register_url = local_server + "/register";
     public static final String login_url = local_server +"/login";
+
+    public static final String NO_USERID = "没有userid";
+    public static final String REGISTER_FAIL = "注册失败";
+    public static final String REGISTER_SUCCESS = "注册成功";
+    public static final String USER_EXISTED = "用户名已存在";
 
 
 }
