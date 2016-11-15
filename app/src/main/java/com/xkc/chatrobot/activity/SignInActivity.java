88d282271b25871a2fe976c9fcadc7f7d3e1e265
xkc <1337154470@qq.com>
2016-11-15 20:04:03 +0800

    1、用户注册成功后将username和password存入Sharedpreferences中，登录时检测，自动填充EditText 2、登陆成功后，跳转至MainActivity时，带上当前用户的参数userid

--- app/src/main/java/com/xkc/chatrobot/activity/SignInActivity.java
+++ app/src/main/java/com/xkc/chatrobot/activity/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.xkc.chatrobot.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
@@ -24,7 +25,7 @@ import java.util.HashMap;
  * Created by xkc on 11/14/16.
  */
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
     private final String TAG = SignInActivity.class.getSimpleName();
     private EditText username_et;
     private EditText password_et;
@@ -33,6 +34,15 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private TextView register_tv;
     private TextView login_tv;
     private TextView go_register_tv;
+
+    //存储已注册用户的登录名和密码
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    //是否已注册
+    private boolean hasRegistered;
+
+    private int userid;
 
 
     @Override
@@ -43,7 +53,29 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
         initEvent();
 
+        preferences = getSharedPreferences("user_info", MODE_PRIVATE);
+        editor = preferences.edit();
+
+        //如果preferences中已经有userid，说明之前已经注册过
+        //将preferences中的username和password读出放到对应的EditText中
+        hasRegistered = preferences.contains("userid");
+
+        initUI();
+
     }
+
+    /**
+     * 如果hasRegistered为true
+     * 将已经注册的username和password从preferences中读出显示在当前UI中
+     */
+    private void initUI() {
+        if (hasRegistered) {
+            username_et.setText(preferences.getString("username", ""));
+            password_et.setText(preferences.getString("password", ""));
+            userid = preferences.getInt("userid",-1);
+        }
+    }
+
 
     private void initEvent() {
         register_tv.setOnClickListener(this);
@@ -69,59 +101,72 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private RegisterCallback registerCallback = new RegisterCallback() {
         @Override
         public void onSuccess(Exception e, Object obj) {
-            if (e == null){
-                Toast.makeText(SignInActivity.this,Const.REGISTER_SUCCESS,Toast.LENGTH_LONG).show();
-            }else {
-                Log.e(TAG,Const.REGISTER_FAIL + obj.toString());
+            if (e == null) {
+                Toast.makeText(SignInActivity.this, Const.REGISTER_SUCCESS, Toast.LENGTH_LONG).show();
+                editor.putInt("userid", (Integer) obj);
+                editor.commit();
+            } else {
+                Log.e(TAG, Const.REGISTER_FAIL + obj.toString());
             }
 
         }
 
         @Override
         public void onFail(Exception e, Object obj) {
-            Log.e(TAG,Const.REGISTER_FAIL+ obj.toString());
+            Log.e(TAG, Const.REGISTER_FAIL + obj.toString());
         }
     };
 
     private LoginCallback loginCallback = new LoginCallback() {
         @Override
         public void onFail(Exception e, Object obj) {
-            Log.e(TAG,Const.LOGIN_FAIL+ obj.toString());
+            Log.e(TAG, Const.LOGIN_FAIL + obj.toString());
         }
 
         @Override
         public void onSuccess(Exception e, Object obj) {
-            if (e == null){
-                Toast.makeText(SignInActivity.this,Const.LOGIN_SUCCESS,Toast.LENGTH_LONG).show();
-                startActivity(new Intent(SignInActivity.this,MainActivity.class));
+            if (e == null) {
+                Toast.makeText(SignInActivity.this, Const.LOGIN_SUCCESS, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                if (userid != -1)
+                    intent.putExtra("userid",userid);
+                startActivity(intent);
+                Log.d(TAG,"userid:"+userid);
                 SignInActivity.this.finish();
-            }else {
-                Log.e(TAG,Const.LOGIN_FAIL + obj.toString());
+            } else {
+                Log.e(TAG, Const.LOGIN_FAIL + obj.toString());
             }
         }
     };
+
     @Override
     public void onClick(View v) {
         String username = username_et.getText().toString().trim();
         String password = password_et.getText().toString().trim();
         String confirm_password = confirm_password_et.getText().toString().trim();
 
-        HashMap<String,String> params = new HashMap<>();
-        params.put("username",username);
-        params.put("password",password);
-        switch (v.getId()){
+        HashMap<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+
+        switch (v.getId()) {
             case R.id.register_tv:
-                RegisterPresenter registerPresenter = new RegisterPresenter(SignInActivity.this,params);
-                if (confirm_password.equals(password)){
+                RegisterPresenter registerPresenter = new RegisterPresenter(SignInActivity.this, params);
+                if (confirm_password.equals(password)) {
                     registerPresenter.doRegister(registerCallback);
-                }else {
-                    Toast.makeText(this,Const.NOT_SAME_PASSWORD,Toast.LENGTH_LONG).show();
+                    editor.putString("username", username);
+                    editor.putString("password", password);
+                    editor.commit();
+
+                } else {
+                    Toast.makeText(this, Const.NOT_SAME_PASSWORD, Toast.LENGTH_LONG).show();
                     confirm_password_et.setText("");
                 }
 
                 break;
             case R.id.login_tv:
-                LoginPresenter loginPresenter = new LoginPresenter(SignInActivity.this,params);
+                LoginPresenter loginPresenter = new LoginPresenter(SignInActivity.this, params);
                 loginPresenter.doLogin(loginCallback);
                 break;
             case R.id.go_register_tv:
@@ -129,6 +174,9 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
                 confirm_password_layout.setVisibility(View.VISIBLE);
                 register_tv.setVisibility(View.VISIBLE);
                 go_register_tv.setVisibility(View.INVISIBLE);
+
+                username_et.setText("");
+                password_et.setText("");
                 break;
         }
 
@@ -136,9 +184,9 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onBackPressed() {
-        if (login_tv.getVisibility() == View.VISIBLE){
+        if (login_tv.getVisibility() == View.VISIBLE) {
             this.finish();
-        }else {
+        } else {
             confirm_password_layout.setVisibility(View.GONE);
             register_tv.setVisibility(View.INVISIBLE);
             login_tv.setVisibility(View.VISIBLE);
