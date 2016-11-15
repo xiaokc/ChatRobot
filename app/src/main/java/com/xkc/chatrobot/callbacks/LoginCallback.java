b61e9544355417b32ae898e91ba967e07ad044e9
xkc <1337154470@qq.com>
2016-11-15 17:34:33 +0800

    使用LoginPresenter和LoginCallback将网络请求抽离出Activity

---
+++ app/src/main/java/com/xkc/chatrobot/callbacks/LoginCallback.java
@@ -0,0 +1,8 @@
+package com.xkc.chatrobot.callbacks;
+
+/**
+ * Created by xkc on 11/15/16.
+ */
+
+public interface LoginCallback {
+}
---
+++ app/src/main/java/com/xkc/chatrobot/presenter/LoginPresenter.java
@@ -0,0 +1,8 @@
+package com.xkc.chatrobot.presenter;
+
+/**
+ * Created by xkc on 11/15/16.
+ */
+
+public class LoginPresenter {
+}
