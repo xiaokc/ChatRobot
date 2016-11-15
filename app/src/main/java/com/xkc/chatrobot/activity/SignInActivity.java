725ad668db50fc3e57937624fd5ba67a8672e7c3
xkc <1337154470@qq.com>
2016-11-15 11:20:31 +0800

    将网络请求的繁重操作抽离到RegisterPresenter中完成，借助MVP思想

--- app/src/main/java/com/xkc/chatrobot/activity/SignInActivity.java
+++ app/src/main/java/com/xkc/chatrobot/activity/SignInActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import com.xkc.chatrobot.Helper.Const;
 import com.xkc.chatrobot.R;
 import com.xkc.chatrobot.callbacks.RegisterCallback;
+import com.xkc.chatrobot.presenter.RegisterPresenter;
 import com.xkc.chatrobot.tasks.MyAsyncTask;
 
 import org.json.JSONException;
@@ -28,6 +29,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 /**
  * Created by xkc on 11/14/16.
@@ -87,136 +89,18 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             Log.e(TAG,Const.REGISTER_FAIL+ obj.toString());
         }
     };
-
-    private void doRegister(final RegisterCallback callback, String username, String password){
-        new MyAsyncTask(this,"register"){
-            @Override
-            protected String doInBackground(String... params) {
-                String username = params[0];
-                String password = params[1];
-
-                String result = "";
-
-                JSONObject jsonObject = new JSONObject();
-                try {
-                    jsonObject.accumulate("username", username);
-                    jsonObject.accumulate("password", password);
-                } catch (JSONException e) {
-                    Log.e(TAG, "Post JSONException occur: " + e.getMessage());
-                }
-
-                String requestParams = jsonObject.toString();
-
-                URL url = null;
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
-                BufferedWriter writer = null;
-
-                Log.i(TAG, "requestParams=" + requestParams);
-                try {
-                    url = new URL(Const.register_url);
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setConnectTimeout(Const.connect_timeout);
-                    connection.setRequestMethod("POST");
-                    connection.setDoOutput(true);
-                    connection.setDoInput(true);
-                    connection.setInstanceFollowRedirects(false);
-                    connection.setRequestProperty("Content-type", "application/json");
-
-                    connection.connect();
-                    writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-                    writer.write(requestParams);
-                    writer.flush();
-
-
-                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String line = "";
-                    while ((line = reader.readLine()) != null) {
-                        result += line;
-                    }
-
-                    Log.i(TAG, "requestParams=" + requestParams + ",result=" + result);
-                } catch (IOException e) {
-                    Log.e(TAG, "IOException occur: " + e.getMessage());
-                } finally {
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-
-                    try {
-                        if (writer != null) {
-                            writer.close();
-                        }
-                    } catch (IOException e) {
-                        Log.e(TAG, "Writer Close IOException occur: " + e.getMessage());
-                    }
-
-
-                    try {
-                        if (reader != null) {
-                            reader.close();
-                        }
-                    } catch (IOException e) {
-                        Log.e(TAG, "Reader Close IOException occur: " + e.getMessage());
-                    }
-
-                }
-
-
-                return result;
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                if (mDialog != null) {
-                    mDialog.dismiss();
-                    mDialog = null;
-                }
-
-                Log.d(TAG,"res:"+s);
-                parseRegisterRes(callback,s);
-
-                super.onPostExecute(s);
-            }
-        }.execute(username,password);
-    }
-
-    /**
-     * 解析注册的json结果
-     * @param s
-     */
-    private void parseRegisterRes(RegisterCallback callback, String s){
-        try {
-            JSONObject jsonObject = new JSONObject(s);
-            if (jsonObject.has("state")){
-                switch (jsonObject.getInt("state")){
-                    case 0:
-                        if (jsonObject.has("userid")){
-                            callback.onSuccess(null,jsonObject.getInt("userid"));
-                        }else {
-                            callback.onFail(new Exception(Const.NO_USERID),Const.NO_USERID);
-                        }
-                        break;
-                    case 1:
-                        callback.onFail(new Exception(Const.REGISTER_FAIL),Const.REGISTER_FAIL);
-                        break;
-                    case 2:
-                        callback.onFail(new Exception(Const.USER_EXISTED),Const.USER_EXISTED);
-                        break;
-                }
-            }
-        } catch (JSONException e) {
-            Log.e(TAG,"JsonException:"+e.getMessage());
-        }
-    }
-
     @Override
     public void onClick(View v) {
         String username = username_et.getText().toString().trim();
         String password = password_et.getText().toString().trim();
+
+        HashMap<String,String> params = new HashMap<>();
+        params.put("username",username);
+        params.put("password",password);
         switch (v.getId()){
             case R.id.register_btn:
-                doRegister(registerCallback,username,password);
+                RegisterPresenter registerPresenter = new RegisterPresenter(SignInActivity.this,params);
+                registerPresenter.doRegister(registerCallback);
                 break;
             case R.id.login_btn:
                 /*doLogin(username,password);*/
---
+++ app/src/main/java/com/xkc/chatrobot/presenter/RegisterPresenter.java
@@ -0,0 +1,167 @@
+package com.xkc.chatrobot.presenter;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.xkc.chatrobot.Helper.Const;
+import com.xkc.chatrobot.callbacks.RegisterCallback;
+import com.xkc.chatrobot.tasks.MyAsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+/**
+ * Created by xkc on 11/15/16.
+ */
+
+public class RegisterPresenter {
+    private Context context;
+    private String username;
+    private String password;
+    private static final String TAG = RegisterPresenter.class.getSimpleName();
+    public RegisterPresenter(Context context, HashMap<String,String> params){
+        this.context = context;
+        if (params != null){
+            if (params.containsKey("username")){
+                username = params.get("username");
+            }
+            if (params.containsKey("password")){
+                password = params.get("password");
+            }
+        }
+
+    }
+
+    public  void doRegister(final RegisterCallback callback){
+        new MyAsyncTask(context,"register"){
+            @Override
+            protected String doInBackground(String... params) {
+                String result = "";
+                if (username == null || password == null){
+                    return Const.NULL_PARAMS;
+                }
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.accumulate("username", username);
+                    jsonObject.accumulate("password", password);
+                } catch (JSONException e) {
+                    Log.e(TAG, "Post JSONException occur: " + e.getMessage());
+                }
+
+                String requestParams = jsonObject.toString();
+
+                URL url = null;
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                BufferedWriter writer = null;
+
+                Log.i(TAG, "requestParams=" + requestParams);
+                try {
+                    url = new URL(Const.register_url);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setConnectTimeout(Const.connect_timeout);
+                    connection.setRequestMethod("POST");
+                    connection.setDoOutput(true);
+                    connection.setDoInput(true);
+                    connection.setInstanceFollowRedirects(false);
+                    connection.setRequestProperty("Content-type", "application/json");
+
+                    connection.connect();
+                    writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+                    writer.write(requestParams);
+                    writer.flush();
+
+
+                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        result += line;
+                    }
+
+                    Log.i(TAG, "requestParams=" + requestParams + ",result=" + result);
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException occur: " + e.getMessage());
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+
+                    try {
+                        if (writer != null) {
+                            writer.close();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Writer Close IOException occur: " + e.getMessage());
+                    }
+
+
+                    try {
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Reader Close IOException occur: " + e.getMessage());
+                    }
+
+                }
+
+
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                if (mDialog != null) {
+                    mDialog.dismiss();
+                    mDialog = null;
+                }
+
+                Log.d(TAG,"res:"+s);
+                parseRegisterRes(callback,s);
+
+                super.onPostExecute(s);
+            }
+        }.execute(username,password);
+    }
+
+    /**
+     * 解析注册的json结果
+     * @param s
+     */
+    private void parseRegisterRes(RegisterCallback callback, String s){
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            if (jsonObject.has("state")){
+                switch (jsonObject.getInt("state")){
+                    case 0:
+                        if (jsonObject.has("userid")){
+                            callback.onSuccess(null,jsonObject.getInt("userid"));
+                        }else {
+                            callback.onFail(new Exception(Const.NO_USERID),Const.NO_USERID);
+                        }
+                        break;
+                    case 1:
+                        callback.onFail(new Exception(Const.REGISTER_FAIL),Const.REGISTER_FAIL);
+                        break;
+                    case 2:
+                        callback.onFail(new Exception(Const.USER_EXISTED),Const.USER_EXISTED);
+                        break;
+                }
+            }else {
+                callback.onFail(new Exception(Const.NULL_PARAMS),"username或password"+Const.NULL_PARAMS);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG,"JsonException:"+e.getMessage());
+        }
+    }
+}
